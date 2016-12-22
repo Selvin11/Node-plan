77b7a6bdf36b034d0424e985916be3b2b2bb55ed
Selvin11 <528370630@qq.com>
2016-12-22 23:40:55 +0800

    signin over

--- models/users.js
+++ models/users.js
@@ -5,5 +5,13 @@ module.exports = {
   // 注册用户
   create: function create(user){
     return User.create(user).exec();
+  },
+  //通过用户名获取用户信息
+  getUserByName: function getUserByName(name){
+    return User
+        .findOne({name:name})
+        //  addCreatedAt 自定义插件（通过 _id 生成时间戳）
+        .addCreatedAt()
+        .exec();
   }
 }
