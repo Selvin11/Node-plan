98de47b9e040d1294962ca0738a1d2c7583af15c
Selvin11 <528370630@qq.com>
2017-01-01 23:08:07 +0800

    add edit

--- routes/posts.js
+++ routes/posts.js
@@ -6,7 +6,7 @@ var PostModel = require('../models/posts');
 // 页面功能和逻辑
 // 首页（显示部分文章，注册以及登录选项）=》注册页面、注册提交跳转页面 =》 用户文章发表页面、发表提交跳转页面 && 用户文章编辑页面、编辑提交跳转页面
 
-// GET /posts?author=xxx 所有用户或者特定用户的文章页
+// GET /posts?author=xxx 所有用户或者特定用户的文章页 
 router.get('/', function(req, res, next) {
   var author = req.query.author;
 
@@ -65,7 +65,7 @@ router.post('/', checkLogin, function(req, res, next) {
 
 // GET /posts/:postId 单独一篇的文章页
 router.get('/:postId', function(req, res, next) {
-  rvar postId = req.params.postId;
+  var postId = req.params.postId;
 
   Promise.all([
     PostModel.getPostById(postId),// 获取文章信息
@@ -86,17 +86,52 @@ router.get('/:postId', function(req, res, next) {
 
 // GET /posts/:postId/edit 更新文章页
 router.get('/:postId/edit', checkLogin, function(req, res, next) {
-  res.send(req.flash());
+  var postId = req.params.postId;
+    var author = req.session.user._id;
+
+    PostModel.getRawPostById(postId)
+      .then(function (post) {
+        if (!post) {
+          throw new Error('该文章不存在');
+        }
+        if (author.toString() !== post.author._id.toString()) {
+          throw new Error('权限不足');
+        }
+        res.render('edit', {
+          post: post
+        });
+      })
+      .catch(next);
 });
 
 // POST /posts/:postId/edit 更新一篇文章
 router.post('/:postId/edit', checkLogin, function(req, res, next) {
-  res.send(req.flash());
+  var postId = req.params.postId;
+    var author = req.session.user._id;
+    var title = req.fields.title;
+    var content = req.fields.content;
+
+    PostModel.updatePostById(postId, author, { title: title, content: content })
+      .then(function () {
+        req.flash('success', '编辑文章成功');
+        // 编辑成功后跳转到上一页
+        res.redirect(`/posts/${postId}`);
+      })
+      .catch(next);
 });
 
 // GET /posts/:postId/remove 删除一篇文章
 router.get('/:postId/remove', checkLogin, function(req, res, next) {
-  res.send(req.flash());
+  var postId = req.params.postId;
+    var author = req.session.user._id;
+
+    PostModel.delPostById(postId, author)
+      .then(function () {
+        req.flash('success', '删除文章成功');
+        // 删除成功后跳转到主页
+        res.redirect('/posts');
+      })
+      .catch(next);
 });
 
 // POST /posts/:postId/comment 创建一条留言
